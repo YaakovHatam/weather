7f38b02c6a817a8e4eab377eb0a950e7cae52eef
yaakov hatam <6986221+YaakovHatam@users.noreply.github.com>
2022-02-04 13:04:22 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,14 +4,17 @@ const getData = async props => {
     if (CITIES_WEATHER_CACHE[props.city]) return CITIES_WEATHER_CACHE[props.city];
 
     builder.init('1e8ffdb64b7d4d8dbdd4bf3a700bdeb0');
+    let cacheKey = '';
     if (props.city) {
         builder.chooseCity(props.city);
+        cacheKey = props.city;
     } else {
         builder.addCoords(props.latitude, props.longitude);
+        cacheKey = props.latitude + ',' + props.longitude;
     }
     builder.chooseUnits(builder.UNITS.METRIC);
     const url = builder.finish();
-    return fetch(url).then(res => res.json()).then(res => (CITIES_WEATHER_CACHE[props.city] = new City(res), CITIES_WEATHER_CACHE[props.city]));
+    return fetch(url).then(res => res.json()).then(res => (CITIES_WEATHER_CACHE[cacheKey] = new City(res), CITIES_WEATHER_CACHE[cacheKey]));
 }
 
 const cardsContainer = document.getElementById('cards');
